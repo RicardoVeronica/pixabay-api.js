a4459d02aa62601a1bbabbf31373290b070c44f5
setcain <charlie.bronson00@gmail.com>
2020-12-28 18:30:23 -0600

    adds show images from api function

--- src/functions/buscarImagenes.js
+++ src/functions/buscarImagenes.js
@@ -3,7 +3,7 @@ import mostrarImagenes from "./mostrarImagenes";
 function buscarImagenes(imagenes) {
   // Busca imagen en la API de Pixabay
   const key = "19694072-3cb3b29e82b493aeef4355860";
-  const url = `https://pixabay.com/api/?key=${key}&q=${imagenes}`;
+  const url = `https://pixabay.com/api/?key=${key}&q=${imagenes}&per_page=100`;
 
   fetch(url)
     .then((resultado) => resultado.json())
--- src/functions/mostrarImagenes.js
+++ src/functions/mostrarImagenes.js
@@ -1,5 +1,47 @@
+import * as UI from "../selectors";
+
 function mostrarImagenes(imagenes) {
   // Mustra las imagenes en el HTML
+  while (UI.resultado.firstChild) {
+    // Elimina resultado anterior
+    UI.resultado.removeChild(UI.resultado.firstChild);
+  }
+
+  imagenes.forEach((imagen) => {
+    const { previewURL, likes, views, largeImageURL } = imagen;
+    UI.resultado.innerHTML += `
+      <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+        <div class="bg-white">
+          <img src="${previewURL}" alt="Image" class="w-full">
+          <div class="p-4">
+            <p class="font-bold">${likes} <span class="font-light">Me Gusta</span></p>
+            <p class="font-bold">${views} <span class="font-light">Vistas</span></p>
+            <a
+              class=
+              "
+                block
+                w-full
+                bg-blue-800
+                hover:bg-blue-500
+                text-white
+                uppercase
+                font-bold
+                text-center
+                rounded
+                mt-5
+                p-1
+              "
+              href="${largeImageURL}"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Ver Imagen
+            </a>
+          </div>
+        </div>
+      </div>
+    `;
+  });
 }
 
 export default mostrarImagenes;
