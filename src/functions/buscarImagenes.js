d499bb51910553a0f2021fc283d2f9c71bc3a7d0
Ricardo Veronica <charlie.bronson00@gmail.com>
2020-12-30 08:23:09 -0600

    Update buscarImagenes.js

--- src/functions/buscarImagenes.js
+++ src/functions/buscarImagenes.js
@@ -2,7 +2,7 @@ import mostrarImagenes from "./mostrarImagenes";
 
 async function buscarImagenes(imagenes) {
   // Busca imagen en la API de Pixabay cambiado promise a async/await
-  const key = "19694072-3cb3b29e82b493aeef4355860";
+  const key = "xxx";
   const url = `https://pixabay.com/api/?key=${key}&q=${imagenes}&per_page=100`;
 
   // fetch(url)
