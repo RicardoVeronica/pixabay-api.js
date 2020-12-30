513d5c818ccb48468b857fd9375af427d2824ab6
setcain <charlie.bronson00@gmail.com>
2020-12-30 08:21:02 -0600

    adds asyncAwait in project

--- src/functions/buscarImagenes.js
+++ src/functions/buscarImagenes.js
@@ -1,15 +1,23 @@
 import mostrarImagenes from "./mostrarImagenes";
 
-function buscarImagenes(imagenes) {
-  // Busca imagen en la API de Pixabay
+async function buscarImagenes(imagenes) {
+  // Busca imagen en la API de Pixabay cambiado promise a async/await
   const key = "19694072-3cb3b29e82b493aeef4355860";
   const url = `https://pixabay.com/api/?key=${key}&q=${imagenes}&per_page=100`;
 
-  fetch(url)
-    .then((resultado) => resultado.json())
-    .then((resultado) => {
-      mostrarImagenes(resultado.hits);
-    });
+  // fetch(url)
+  //   .then((resultado) => resultado.json())
+  //   .then((resultado) => {
+  //     mostrarImagenes(resultado.hits);
+  //   });
+
+  try {
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+    mostrarImagenes(resultado.hits);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export default buscarImagenes;
